3f053a7d9b143ad996c026b1665d449ac4dffcb6
DnT-Nutan <nutan.g@dntinfotech.com>
2025-02-14 15:07:58 +0530

    Update vnet.tf

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -2,23 +2,24 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = var.vnet_address_space
+  address_space       = [var.vnet_address_space] # Ensure it's a list
+
+  depends_on = [azurerm_resource_group.rg] # Ensures RG exists
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.subnet_address_prefix
+  address_prefixes     = [var.subnet_address_prefix] # Ensure it's a list
 
   depends_on = [azurerm_virtual_network.vnet]
 }
 
-
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = var.location
-  resource_group_name   = var.resource_group_name
+  resource_group_name = var.resource_group_name
 
   # Allow SSH traffic on port 22
   security_rule {
@@ -61,7 +62,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "new-nic"
+  name                = var.nic_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -71,9 +72,16 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip.id  
   }
+
+  depends_on = [azurerm_subnet.subnet]
 }
 
 resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
+
+  depends_on = [
+    azurerm_network_interface.nic,
+    azurerm_network_security_group.nsg
+  ]
 }
