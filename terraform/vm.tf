0f8bea9e4a649d4ee361a875dddafa39a2f9d622
DnT-Nutan <nutan.g@dntinfotech.com>
2025-02-17 11:52:56 +0530

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,56 +1,48 @@
-# Create Public IPs for VMs
 resource "azurerm_public_ip" "vm_public_ip" {
-  count               = var.vm_count
-  name                = var.vm_public_ip_names[count.index]
+  count                = length(var.vm_names)
+  name                 = "public-ip-${var.vm_names[count.index]}"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name  
   allocation_method   = var.public_ip_allocation_method
   sku                 = "Standard"
 }
 
-# Create Network Interfaces for VMs
-resource "azurerm_network_interface" "nic" {
-  count               = var.vm_count
-  name                = "nic-${count.index}"
+resource "azurerm_virtual_machine" "vm" {
+  count                = length(var.vm_names)
+  name                 = var.vm_names[count.index]
   location            = var.location
   resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "ipconfig-${count.index}"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip[count.index].id
-  }
-}
-
-# Create Virtual Machines
-resource "azurerm_linux_virtual_machine" "vm" {
-  count                 = var.vm_count
-  name                  = var.vm_names[count.index]
-  location              = var.location
-  resource_group_name   = var.resource_group_name
   network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-  size                  = var.vm_size
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  disable_password_authentication = true
+  vm_size             = var.vm_size
 
-  # SSH Configuration
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file("/home/dnt/az-master_public_key.pub")
+  # OS Profile
+  os_profile {
+    computer_name  = var.vm_names[count.index]
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  # Linux Configuration
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    # Specify the SSH public key
+    ssh_keys {
+      path     = "/home/ubuntu/.ssh/authorized_keys"
+      key_data = file("/home/dnt/az-master_public_key.pub")
+    }
   }
 
   # OS Disk
-  os_disk {
-    name                 = var.os_disk_names[count.index]
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = 30
+  storage_os_disk {
+    name          = "${var.vm_names[count.index]}-os-disk"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+    disk_size_gb  = 30
   }
 
-  # Image Reference (Ubuntu)
-  source_image_reference {
+  # Image Reference for Ubuntu
+  storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
     sku       = var.image_sku
