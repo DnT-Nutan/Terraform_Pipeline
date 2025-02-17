8c92bf08b2a150dbdf2c3570dc82cd53c24ff9a1
DnT-Nutan <nutan.g@dntinfotech.com>
2025-02-17 16:41:29 +0530

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,6 +1,5 @@
 resource "azurerm_public_ip" "vm_public_ip" {
-  count                = length(var.vm_names)
-  name                 = "public-ip-${var.vm_names[count.index]}"
+  name                = var.vm_public_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name  
   allocation_method   = var.public_ip_allocation_method
@@ -8,16 +7,15 @@ resource "azurerm_public_ip" "vm_public_ip" {
 }
 
 resource "azurerm_virtual_machine" "vm" {
-  count                = length(var.vm_names)
-  name                 = var.vm_names[count.index]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic[count.index % length(azurerm_network_interface.nic)].id]
-  vm_size             = var.vm_size
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
 
   # OS Profile
   os_profile {
-    computer_name  = var.vm_names[count.index]
+    computer_name  = var.vm_name
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
@@ -29,13 +27,13 @@ resource "azurerm_virtual_machine" "vm" {
     # Specify the SSH public key
     ssh_keys {
       path     = "/home/ubuntu/.ssh/authorized_keys"
-      key_data = file("/home/dnt/az-master_public_key.pub")
+      key_data = file("/home/dnt/az-master_public_key.pub") 
     }
   }
 
   # OS Disk
   storage_os_disk {
-    name          = "${var.vm_names[count.index]}-os-disk"
+    name          = var.os_disk_name
     caching       = "ReadWrite"
     create_option = "FromImage"
     disk_size_gb  = 30
@@ -49,3 +47,4 @@ resource "azurerm_virtual_machine" "vm" {
     version   = var.image_version
   }
 }
+
