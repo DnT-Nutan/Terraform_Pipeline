fcdd22efe37af2623e11a4500c5513736bd08e0a
DnT-Nutan <nutan.g@dntinfotech.com>
2025-02-18 11:54:30 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "newton-resources"
+  default     = "samsung-resources"
 }
 
 # Variable for the location
