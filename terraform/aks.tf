7333cd561a33bcad63ee0ceac0176276eeabe938
DnT-Nutan <nutan.g@dntinfotech.com>
2025-02-18 11:41:29 +0530

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "public_subnet" {
   name                 = var.subnet_new_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet[0].name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.public_subnet_address_prefix]
 }
 
@@ -12,39 +12,37 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.aks_dns_name_prefix
 
   default_node_pool {
-    name            = var.aks_node_pool_name
-    node_count      = var.aks_node_pool_count
-    vm_size         = var.aks_node_pool_vm_size
-    vnet_subnet_id  = azurerm_subnet.public_subnet.id
+    name       = var.aks_node_pool_name
+    node_count = var.aks_node_pool_count
+    vm_size    = var.aks_node_pool_vm_size
+    vnet_subnet_id = azurerm_subnet.public_subnet.id
   }
 
   identity {
-    type = "SystemAssigned"
+    type = "SystemAssigned" 
   }
 
   linux_profile {
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = file("/home/dnt/az-master_public_key.pub")
+      key_data = file("/home/dnt/az-master_public_key.pub")  
     }
   }
 
   network_profile {
     network_plugin = "azure"
     network_policy = "azure"
-    service_cidr   = "10.1.0.0/16"
+    service_cidr   = "10.1.0.0/16"  
     dns_service_ip = "10.1.0.10"
   }
-  
-  depends_on = [azurerm_container_registry.new_acr]
+  depends_on = [azurerm_container_registry.new_acr]  
 }
-
 # Grant AKS Managed Identity access to ACR using Azure Role Assignment
 resource "azurerm_role_assignment" "acrpull_role" {
   scope                            = azurerm_container_registry.new_acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.identity.0.principal_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.identity[0].principal_id
   skip_service_principal_aad_check = true
 }
 
